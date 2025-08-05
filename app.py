70c13041b813fc4e16459deb82c3ea41c8532cb2
Wail310 <wailsangman98@gmail.com>
2025-08-06 01:37:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -29,6 +29,7 @@ def handle_message(event):
         event.reply_token,
         TextSendMessage(text=response_message)
     )
+    
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 
-if __name__ == "__main__":
-    app.run()
